0ce5dd2ff0b1ff5b0edf2010dcc41cfa27e84b25
Charles Pasiak <charlespasiak@gmail.com>
2023-06-30 11:28:04 +0700

    add function verifyUserCredential in UsersService.js

--- src/services/UsersService.js
+++ src/services/UsersService.js
@@ -40,6 +40,29 @@ class UsersService {
       throw new InvariantError('Gagal menambahkan user. Username sudah digunakan.');
     }
   }
+
+  async verifyUserCredential(username, password) {
+    const query = {
+      text: 'SELECT id, password FROM users WHERE username = $1',
+      values: [username],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new InvariantError('Kredensial yang Anda berikan salah');
+    }
+
+    const { id, password: hashedPassword } = result.rows[0];
+
+    const match = await bcrypt.compare(password, hashedPassword);
+
+    if (!match) {
+      throw new InvariantError('Kredensial yang Anda berikan salah');
+    }
+
+    return id;
+  }
 }
 
 module.exports = UsersService;
