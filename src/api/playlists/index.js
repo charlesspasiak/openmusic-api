674865f3383f3a93ab21fa2180e702e4a58c5281
Charles Pasiak <charlespasiak@gmail.com>
2023-07-01 11:43:24 +0700

    register plugin to server.js

---
+++ src/api/playlists/index.js
@@ -0,0 +1,12 @@
+const routes = require('./routes');
+const PlaylistsHandler = require('./handler');
+
+module.exports = {
+  name: 'playlists',
+  version: '1.0.0',
+  register: async (server, { playlistsService, songsService, validator }) => {
+    const playlistsHandler = new PlaylistsHandler(playlistsService, songsService, validator);
+
+    server.route(routes(playlistsHandler));
+  },
+};
